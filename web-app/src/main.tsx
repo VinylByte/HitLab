629f766c6a8c96d5ff890241919948b96583775b
Deimon132 <67980709+Deimon132@users.noreply.github.com>
2026-02-25 12:55:30 +0100

    Hero UI Init

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 1. import `HeroUIProvider` component
+import {HeroUIProvider} from "@heroui/react";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
