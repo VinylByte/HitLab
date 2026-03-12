f60707a7eb7e295f61ce6a2ff9875556d30167b2
Michael Stepanov <michael05.stepanov@gmail.com>
2026-03-12 11:52:52 +0100

    moved some files for structure

--- web-app/src/types/deck.ts
+++ web-app/src/types/deck.ts
@@ -1,4 +1,4 @@
-import type { Tables } from "../database.types";
+import type { Tables } from "./database.types";
 import type { DeckOwner } from "./profile";
 
 export type DeckRow = Tables<"decks">;
